46accba2d4c79063f304a861d55baef70aa1a551
flakeforever <113103193+flakeforever@users.noreply.github.com>
2024-11-24 01:00:02 +0800

    mondrian: Evolution-X 10.0 bringup

--- lineage_mondrian.mk
+++ lineage_mondrian.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022-2023 The LineageOS Project
+# Copyright (C) 2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -18,16 +18,14 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_SYSTEM_NAME := mondrian_global
 PRODUCT_SYSTEM_DEVICE := mondrian
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="mondrian_global-user 12 SKQ1.230401.001 V816.0.10.0.UMNMIXM release-keys" \
-    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
-    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
-
-BUILD_FINGERPRINT := POCO/mondrian_global/mondrian:12/SKQ1.230401.001/V816.0.10.0.UMNMIXM:user/release-keys
-
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="mondrian_global-user 12 SKQ1.230401.001 V816.0.10.0.UMNMIXM release-keys" \
+    BuildFingerprint=POCO/mondrian_global/mondrian:12/SKQ1.230401.001/V816.0.10.0.UMNMIXM:user/release-keys \
+    DeviceProduct=$(PRODUCT_DEVICE)
+
 TARGET_BOOT_ANIMATION_RES := 1440
-# Evolution-XYZ
+
 WITH_GMS := true
 TARGET_HAS_UDFPS := true
