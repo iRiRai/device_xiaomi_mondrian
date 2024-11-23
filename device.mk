a01025892e2dc4f33f74b7aafbcdc501e607a7bd
flakeforever <113103193+flakeforever@users.noreply.github.com>
2024-11-24 01:03:57 +0800

    mondrian: Update USB HAL service for Android 15

--- device.mk
+++ device.mk
@@ -549,8 +549,16 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti \
+    android.hardware.usb-service.qti \
+    android.hardware.usb.gadget-service.qti \
     libusbhost.vendor
+
+PRODUCT_PACKAGES += \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh \
+    usb_compositions.conf
+
+PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/usb/etc
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
